39d0a8cb8725e0a4d7b3cac608704cf6fbd52887
Liliia Nikitchenko <liliia.nikitchenko@hec.de>
2025-10-08 11:35:26 +0200

    feat: Mapping Service created

---
+++ src/main/java/de/szut/lf8_starter/mapper/MappingService.java
@@ -0,0 +1,10 @@
+package de.szut.lf8_starter.mapper;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MappingService {
+    void mapAddProjectDTOtoProjectEntity(){
+
+    }
+}
