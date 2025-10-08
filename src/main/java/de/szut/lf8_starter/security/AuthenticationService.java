e1695a3a7ad0ac49c324c484dec90fed8fcc0ec6
Jarne Rosenbaum <jarne.rosenbaum@hec.de>
2025-10-08 11:11:13 +0200

    feat: added AuthenticationService with getJwt Method

---
+++ src/main/java/de/szut/lf8_starter/security/AuthenticationService.java
@@ -0,0 +1,22 @@
+package de.szut.lf8_starter.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+    public String getCurrentJWT() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication instanceof JwtAuthenticationToken jwtAuthenticationToken) {
+            Jwt jwt = jwtAuthenticationToken.getToken();
+            return jwt.getTokenValue();
+        }
+
+        return null;
+    }
+}
