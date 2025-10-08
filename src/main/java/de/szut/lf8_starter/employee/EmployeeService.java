30f6c4adadd5253195027638fdecc174d2b86a94
Marvin Meiling <marvin.meiling@hec.de>
2025-10-08 12:49:57 +0200

    feat: Added JWT-Authorization in EmployeeService

--- src/main/java/de/szut/lf8_starter/employee/EmployeeService.java
+++ src/main/java/de/szut/lf8_starter/employee/EmployeeService.java
@@ -1,20 +1,38 @@
 package de.szut.lf8_starter.employee;
 
-import org.springframework.http.HttpStatus;
+import de.szut.lf8_starter.security.AuthenticationService;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
 
     private final RestTemplate restTemplate;
-    private String url = "https://employee.szut.dev/employees";
+    private String url = "https://employee-api.szut.dev/employees";
 
     public EmployeeService() {
         this.restTemplate = new RestTemplate();
     }
 
-    public HttpStatus checkIfEmployeeExists(Long id) {
-        return this.restTemplate.getForObject(this.url + "/" + id, HttpStatus.class);
+    private HttpEntity<Void> getHttpEntityWithToken() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(AuthenticationService.getCurrentJWT());
+        return new HttpEntity<>(headers);
+    }
+
+    public boolean checkIfEmployeeExists(Long id) {
+        HttpEntity<Void> entity = getHttpEntityWithToken();
+        String url = this.url + "/" + id;
+
+        ResponseEntity<Boolean> response = this.restTemplate.exchange(url, HttpMethod.GET, entity, Boolean.class);
+
+        if (response.getStatusCode() == HttpStatus.OK) {
+            return true;
+        }
+
+        return false;
     }
 }
