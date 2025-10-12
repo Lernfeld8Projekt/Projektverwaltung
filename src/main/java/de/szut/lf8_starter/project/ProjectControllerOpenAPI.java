113bc02f0068ef05626c3b5de9b7513a5178855f
Jarne <jarne5478@gmail.com>
2025-10-12 11:46:18 +0200

    SCRUM-48 feat: created swagger documentation for patch endpoint

--- src/main/java/de/szut/lf8_starter/project/ProjectControllerOpenAPI.java
+++ src/main/java/de/szut/lf8_starter/project/ProjectControllerOpenAPI.java
@@ -4,6 +4,7 @@ import de.szut.lf8_starter.examples.hello.dto.HelloCreateDto;
 import de.szut.lf8_starter.examples.hello.dto.HelloGetDto;
 import de.szut.lf8_starter.project.DTO.AddProjectDTO;
 import de.szut.lf8_starter.project.DTO.GetProjectDTO;
+import de.szut.lf8_starter.project.DTO.PatchProjectDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -12,7 +13,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 
 public interface ProjectControllerOpenAPI {
-    @Operation(summary = "creates a new project with its id, title," +
+    @Operation(summary = "creates a new project with it's id, title," +
             " responsible employee id, customer id, customer representative name," +
             " goal, start date, planned end date and actual end date")
     @ApiResponses(value = {
@@ -22,6 +23,21 @@ public interface ProjectControllerOpenAPI {
             @ApiResponse(responseCode = "400", description = "invalid JSON posted",
                     content = @Content),
             @ApiResponse(responseCode = "401", description = "not authorized",
-                    content = @Content)})
+                    content = @Content)
+    })
     ResponseEntity<GetProjectDTO> createProject(AddProjectDTO addProjectDTO);
+
+    @Operation(summary = "patches an existing project by it's id with it's title," +
+            " responsible employee id, customer id, customer representative name," +
+            " goal, start date, planned end date and actual end date")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "patched project",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = GetProjectDTO.class))}),
+            @ApiResponse(responseCode = "400", description = "invalid JSON posted",
+                    content = @Content),
+            @ApiResponse(responseCode = "401", description = "not authorized",
+                    content = @Content),
+    })
+    ResponseEntity<GetProjectDTO> updateProject(Long id,PatchProjectDTO patchProjectDTO);
 }
